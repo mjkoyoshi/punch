cf243366381f8a0bedc57749f93779bfca949038
mjkoyoshi <yusuke.koyoshi@majors.co.jp>
2020-09-17 11:55:47 +0900

    recfact:function

--- script/vpnconnect.bash
+++ script/vpnconnect.bash
@@ -8,15 +8,32 @@ fi
 
 vpn="${1:-${VPN_NAME}}"
 
-function isnt_connected () {
-    scutil --nc status "$vpn" | sed -n 1p | grep -qv "Connected"
+function is_connected() {
+    scutil --nc status "$1" | sed -n 1p | grep -qv "Connected"
 }
 
-function poll_until_connected () {
+function is_disconnected() {
+    scutil --nc status "$1" | sed -n 1p | grep -q "Disconnected"
+}
+
+function poll_until_connected() {
     let loops=0 || true
     let max_loops=200 # 200 * 0.1 is 20 seconds. Bash doesn't support floats
 
-    while isnt_connected "$vpn"; do
+    while is_connected "$vpn"; do
+        sleep 0.1 # can't use a variable here, bash doesn't have floats
+        let loops=$loops+1
+        [ $loops -gt $max_loops ] && break
+    done
+
+    [ $loops -le $max_loops ]
+}
+
+function poll_until_disconnected() {
+    let loops=0 || true
+    let max_loops=200 # 200 * 0.1 is 20 seconds. Bash doesn't support floats
+
+    while is_disconnected "$vpn"; do
         sleep 0.1 # can't use a variable here, bash doesn't have floats
         let loops=$loops+1
         [ $loops -gt $max_loops ] && break
@@ -42,15 +59,17 @@ function connect() {
 function disconnect() {
     echo Disconnecting "$vpn" ...
     networksetup -disconnectpppoeservice "$vpn"
+
+    if poll_until_disconnected "$vpn"; then
+        echo "Disconnected to $vpn!"
+        return 0
+    else
+        echo "I'm too impatient!"
+        return 1
+    fi
 }
 
-function is_disconnected() {
-    scutil --nc status "$vpn" | sed -n 1p | grep -q "Disconnected"
-    return $?
-}
-
-is_disconnected
-if [ $? -eq 0 ]; then
+if is_disconnected "$vpn"; then
     connect
 else
     disconnect
